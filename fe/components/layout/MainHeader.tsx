9e0f9db2ff122df8597af02085158c245f7efa62
gbao4132 <89295179+gbao4132@users.noreply.github.com>
2026-09-14 17:12:28 +0700

    Sửa lại icon dropdown và làm cho subnav trở nên responsive hơn

--- fe/components/layout/MainHeader.tsx
+++ fe/components/layout/MainHeader.tsx
@@ -4,17 +4,15 @@ export default function MainHeader() {
   return (
     <div className="bg-[#0046be] text-white">
       <div className="mx-auto flex h-16 max-w-[1600px] items-center gap-4 px-4">
-        {/* Logo */}
         <Link href="/" className="shrink-0">
           <BaoShopLogo className="h-auto w-16" />
         </Link>
-
         {/* Search bar */}
         <div className="flex flex-1 items-center">
-          <div className="flex w-full max-w-3xl overflow-hidden rounded-md bg-white">
+          <div className="flex w-full overflow-hidden rounded-md bg-white">
             <input
               type="text"
-              placeholder="Search Best Buy"
+              placeholder="Tìm kiếm sản phẩm, thương hiệu và nhiều hơn nữa"
               className="flex-1 px-4 py-2 text-sm text-gray-800 outline-none placeholder:text-gray-500"
             />
             <button
@@ -41,23 +39,6 @@ export default function MainHeader() {
 
         {/* Right side: store / account / cart */}
         <div className="flex shrink-0 items-center gap-6">
-          <Link href="/stores" className="flex items-center gap-2 hover:underline">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-              role="img"
-              className="h-6 w-6 fill-current text-white"
-            >
-              <path d="M19.5 22H4.25C3.01 22 2 20.99 2 19.75v-10c0-.41.34-.75.75-.75s.75.34.75.75v10c0 .41.34.75.75.75H19.5c.55 0 1-.45 1-1v-8.25c0-.41-.34-.75-.75-.75h-3c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h3C20.99 9 22 10.01 22 11.25v8.25a2.5 2.5 0 0 1-2.5 2.5" />
-              <path d="M16.25 14.5h-8.5c-.41 0-.75.34-.75.75v6c0 .41.34.75.75.75s.75-.34.75-.75V16h2.75v5.25c0 .41.34.75.75.75s.75-.34.75-.75V16h2.75v5.25c0 .41.34.75.75.75s.75-.34.75-.75v-6c0-.41-.34-.75-.75-.75M15.75 12.5H5.25a.75.75 0 0 1-.53-.22l-3-3a.75.75 0 0 1-.22-.53v-3.5c0-.2.08-.39.22-.53l3-3a.75.75 0 0 1 .53-.22h10.5c.96 0 1.75.79 1.75 1.75v7.5c0 .96-.79 1.75-1.75 1.75M5.56 11h10.19c.14 0 .25-.11.25-.25v-7.5c0-.14-.11-.25-.25-.25H5.56L3 5.56v2.88z" />
-              <path d="M6 7c0 .55-.45 1-1 1s-1-.45-1-1 .45-1 1-1 1 .45 1 1" />
-            </svg>
-            <div className="flex flex-col text-xs leading-tight">
-              <span>Your store</span>
-              <span className="font-semibold">Aiea</span>
-            </div>
-          </Link>
 
           <Link href="/account" className="flex items-center gap-2 hover:underline">
             <svg
@@ -77,27 +58,26 @@ export default function MainHeader() {
             </svg>
 
             <div className="flex flex-col text-xs leading-tight">
-              <span>Account</span>
-              <span className="font-semibold">Sign in</span>
+              <span>Tài khoản</span>
+              <span className="font-semibold">Đăng nhập</span>
             </div>
           </Link>
 
           <Link href="/cart" aria-label="Cart" className="hover:opacity-80">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7"
-              fill="none"
+              xmlSpace="preserve"
               viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
+              aria-hidden="true"
+              role="img"
+              aria-label="Cart Icon"
+              fill="white"
+              height={24}
+              width={24}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 3h2l2.4 12.4a2 2 0 002 1.6h7.7a2 2 0 002-1.6L21 7H6"
-              />
-              <circle cx="9" cy="20" r="1.5" />
-              <circle cx="17" cy="20" r="1.5" />
+              <path d="M9 22.5c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3M9 18c-.83 0-1.5.67-1.5 1.5S8.17 21 9 21s1.5-.67 1.5-1.5S9.83 18 9 18m10 4.5c-1.65 0-3-1.35-3-3s1.35-3 3-3 3 1.35 3 3-1.35 3-3 3m0-4.5c-.83 0-1.5.67-1.5 1.5S18.17 21 19 21s1.5-.67 1.5-1.5S19.83 18 19 18" />
+              <path d="M18.75 18h-11c-1.24 0-2.25-1.01-2.25-2.25s1.01-2.25 2.25-2.25h11.2c.12 0 .22-.08.24-.2l1.28-5.78c.08-.37-.01-.76-.25-1.05S19.64 6 19.26 6H5c-.41 0-.75-.34-.75-.75s.34-.75.75-.75h14.26c.84 0 1.62.38 2.14 1.03.53.65.72 1.5.54 2.32l-1.28 5.78c-.18.81-.88 1.37-1.71 1.37H7.75c-.41 0-.75.34-.75.75s.34.75.75.75h11c.41 0 .75.34.75.75s-.34.75-.75.75" />
+              <path d="M7.25 15c-.34 0-.64-.23-.73-.57L3.9 3.95A1.25 1.25 0 0 0 2.69 3h-.94C1.34 3 1 2.66 1 2.25s.34-.75.75-.75h.94c1.26 0 2.36.86 2.67 2.08l2.62 10.49a.748.748 0 0 1-.73.93" />
             </svg>
           </Link>
         </div>
--- fe/components/layout/SubNav.tsx
+++ fe/components/layout/SubNav.tsx
@@ -45,11 +45,16 @@ export default function SubNav({ visible }: SubNavProps) {
               {item.hasDropdown && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-3 w-3"
-                  viewBox="0 0 20 20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  role="img"
                   fill="currentColor"
+                  className="block h-4 w-4"
                 >
-                  <path d="M5.5 7l4.5 5 4.5-5H5.5z" />
+                  <path
+                    fillRule="evenodd"
+                    d="m12.71 16.29 5.59-5.59c.63-.63.18-1.71-.71-1.71H6.41c-.89 0-1.34 1.08-.71 1.71l5.59 5.59c.39.39 1.02.39 1.41 0Z"
+                  />
                 </svg>
               )}
             </Link>
@@ -57,7 +62,7 @@ export default function SubNav({ visible }: SubNavProps) {
         </div>
 
         {/* Right: plain links */}
-        <div className="flex flex-1 items-center justify-end gap-5">
+        <div className="flex flex-1 items-center justify-end gap-5 overflow-hidden">
           {linkItems.map((item) => (
             <Link
               key={item.label}
@@ -68,11 +73,16 @@ export default function SubNav({ visible }: SubNavProps) {
               {item.hasDropdown && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-3 w-3"
-                  viewBox="0 0 20 20"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  role="img"
                   fill="currentColor"
+                  className="block h-3 w-3"
                 >
-                  <path d="M5.5 7l4.5 5 4.5-5H5.5z" />
+                  <path
+                    fillRule="evenodd"
+                    d="m12.71 16.29 5.59-5.59c.63-.63.18-1.71-.71-1.71H6.41c-.89 0-1.34 1.08-.71 1.71l5.59 5.59c.39.39 1.02.39 1.41 0Z"
+                  />
                 </svg>
               )}
             </Link>
